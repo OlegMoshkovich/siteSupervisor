f499c0430119e404e2aed8c2d37e92ecd129223c
olegmoshkovich <oleg.mosh@gmail.com>
2025-07-06 18:25:39 +0200

    sort out the auth

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Alert, StyleSheet, View, AppState, TouchableOpacity, Text, Image, Dimensions } from 'react-native'
+import { Alert, StyleSheet, View, AppState, TouchableOpacity, Text, Image, Dimensions, Keyboard, TouchableWithoutFeedback } from 'react-native'
 import { supabase } from '../lib/supabase'
 import { Input } from '@rneui/themed'
 import colors from './colors'
@@ -48,67 +48,69 @@ export default function Auth() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={{ position: 'absolute', top: -30, left: 0, width: '100%', alignItems: 'center', zIndex: 1 }} pointerEvents="none">
-        <Image
-          source={require('../assets/cloneit.png')}
-          style={{ width: width , height: 50, resizeMode: 'contain' }}
-        />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <View style={{ position: 'absolute', top: -30, left: 0, width: '100%', alignItems: 'center', zIndex: 1 }} pointerEvents="none">
+          <Image
+            source={require('../assets/cloneit.png')}
+            style={{ width: width , height: 50, resizeMode: 'contain' }}
+          />
                
+        </View>
+        <View style={[styles.verticallySpaced, styles.mt20]}>
+          <Input
+            label="Email"
+            onChangeText={(text) => setEmail(text)}
+            value={email}
+            placeholder="Email"
+            autoCapitalize={'none'}
+            style={{  fontSize: 16, borderColor: colors.primary, paddingVertical: 12, backgroundColor: 'white' }}
+            labelStyle={{  display: 'none' }}
+          />
+        </View>
+        <View style={styles.verticallySpaced}>
+          <Input
+            label="Password"
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+            secureTextEntry={true}
+            placeholder="Password"
+            autoCapitalize={'none'}
+            labelStyle={{  display: 'none' }}
+            style={{  fontSize: 16, borderColor: colors.primary, paddingVertical: 12, backgroundColor: 'white' }}
+          />
+        </View>
+        <View style={[styles.verticallySpaced, {width: '60%', alignSelf: 'center', marginTop: 20}]}>
+          <TouchableOpacity
+              style={[styles.customButton,
+            {
+              backgroundColor: colors.secondary,
+              shadowColor: '#000',
+              shadowOffset: { width: 3, height: 3 },
+              shadowOpacity: 0.2,
+              shadowRadius: 6,
+              elevation: 8,
+              justifyContent: 'center',
+              alignItems: 'center',
+              }]}
+            disabled={loading}
+            onPress={signInWithEmail}
+          >
+            <Text style={styles.buttonText}>Sign in</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.verticallySpaced}>
+          <TouchableOpacity
+            style={[styles.customButton, { width: '60%', alignSelf: 'center',  borderColor: colors.primary, marginTop: 0}]}
+            disabled={loading}
+            onPress={signUpWithEmail}
+          >
+            <Text style={[styles.buttonText, { color: '#009FE3', fontSize: 16 }]}>Sign up</Text>
+          </TouchableOpacity>
+        </View>
+        
       </View>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input
-          label="Email"
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          placeholder="Email"
-          autoCapitalize={'none'}
-          style={{  fontSize: 16, borderColor: colors.primary, paddingVertical: 12, backgroundColor: 'white' }}
-          labelStyle={{  display: 'none' }}
-        />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          label="Password"
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          secureTextEntry={true}
-          placeholder="Password"
-          autoCapitalize={'none'}
-          labelStyle={{  display: 'none' }}
-          style={{  fontSize: 16, borderColor: colors.primary, paddingVertical: 12, backgroundColor: 'white' }}
-        />
-      </View>
-      <View style={[styles.verticallySpaced, {width: '60%', alignSelf: 'center', marginTop: 20}]}>
-        <TouchableOpacity
-            style={[styles.customButton,
-          {
-            backgroundColor: colors.secondary,
-            shadowColor: '#000',
-            shadowOffset: { width: 3, height: 3 },
-            shadowOpacity: 0.2,
-            shadowRadius: 6,
-            elevation: 8,
-            justifyContent: 'center',
-            alignItems: 'center',
-            }]}
-          disabled={loading}
-          onPress={signInWithEmail}
-        >
-          <Text style={styles.buttonText}>Sign in</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.verticallySpaced}>
-        <TouchableOpacity
-          style={[styles.customButton, { width: '60%', alignSelf: 'center',  borderColor: colors.primary, marginTop: 0}]}
-          disabled={loading}
-          onPress={signUpWithEmail}
-        >
-          <Text style={[styles.buttonText, { color: '#009FE3', fontSize: 16 }]}>Sign up</Text>
-        </TouchableOpacity>
-      </View>
-      
-    </View>
+    </TouchableWithoutFeedback>
   )
 }
 
