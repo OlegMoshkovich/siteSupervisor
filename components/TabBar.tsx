5cbe182937690142412ecd02359cf227fae90dd0
olegmoshkovich <oleg.mosh@gmail.com>
2025-07-08 14:02:18 +0200

    tab styles

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -12,7 +12,7 @@ const TabBar: React.FC<TabBarProps> = ({ activeTab, setActiveTab, uploading }) =
   <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', marginBottom: 6, marginTop: 6, width: '90%', alignSelf: 'center', borderRadius: 20, paddingBottom: 0 }}>
     {/* <TabButton icon="document-text" label="Notes" active={activeTab === 'notes'} onPress={() => setActiveTab('notes')} /> */}
     <TabButton icon="camera" label="Photos" active={activeTab === 'photos'} onPress={() => setActiveTab('photos')} disabled={uploading} />
-    <TabButton icon="book" label="Summaries" active={activeTab === 'summaries'} onPress={() => setActiveTab('summaries')} />
+    <TabButton icon="document-text" label="Reports" active={activeTab === 'summaries'} onPress={() => setActiveTab('summaries')} />
   </View>
 );
 
